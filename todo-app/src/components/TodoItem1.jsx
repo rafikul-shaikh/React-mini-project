193c31fdcefa9761bf74674bff03967ead484c94
Rafikul shaikh <rafikulshaikh510@gmail.com>
2025-11-03 23:08:44 +0530

    todo-app folder added

---
+++ todo-app/src/components/TodoItem1.jsx
@@ -0,0 +1,18 @@
+function TodoItem1() {
+  let todoName = "Buy Milk";
+  let todoDate = "03/11/25";
+  return (
+    <div class="container text-center">
+      <div class="row">
+        <div class="col-6">{todoName}</div>
+        <div class="col-4">{todoDate} </div>
+        <div class="col-2">
+          <button type="button" class="btn btn-danger">
+            delete
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default TodoItem1;
